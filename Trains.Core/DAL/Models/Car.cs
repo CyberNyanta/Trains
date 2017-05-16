5dba5889ecf0630b57ab56f1b300b3bce47efab2
Eugene Sianko <eugenesianko@gmail.com>
2017-05-16 07:12:36 +0300

    init

---
+++ Trains.Core/DAL/Models/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trains.Core.DAL.Models
+{
+    public class PassengerCar
+    {
+        public int Id{ get; set; }
+        public int SeatsCount { get; set; }
+        public int ReservationsCount { get; set; }
+        public int AvailableSeatsCount { get; set; }
+
+    }
+}
---
+++ Trains.Core/DAL/Models/Train.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trains.Core.DAL.Models
+{
+    public class Train
+    {
+        public int Number { get; set; }
+        public string Destination { get; set; }        
+        public DateTime DepartureDate { get; set; }
+        public DateTime ArrivalDate { get; set; }
+        public int CarCount { get; set; }
+        public int BookingCount { get; set; }
+    }
+}
